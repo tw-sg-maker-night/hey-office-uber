c89bf4f8d8f915613ba85f045093c473817393fd
Albert Salim <asalim@thoughtworks.com>
2017-01-28 21:53:39 +0700

    Create uber client

---
+++ spec/lib/uber-client.js
@@ -0,0 +1,47 @@
+const uberClient = require('../../lib/uber-client')
+const expect = require('chai').expect
+const nock = require('nock')
+
+const startLatitude = 1.290270
+const startLongitude = 103.851959
+
+describe('get arrival time estimate for all services', () => {
+  setupNockResponse = () => {
+    nock('https://api.uber.com')
+      .get(`/v1.2/estimates/time?start_latitude=${startLatitude}&start_longitude=${startLongitude}`)
+      .replyWithFile(200, __dirname + '/../../samples/responses/time-estimate/all.json')
+  }
+
+  it('should return a list of uber services', (done) => {
+    setupNockResponse()
+
+    uberClient.getArrivalTime(startLatitude, startLongitude).then(services => {
+      expect(services.length).to.eql(5)
+      done()
+    })
+  })
+
+  it('should return the service names and arrival time estimates', (done) => {
+    setupNockResponse()
+    
+    uberClient.getArrivalTime(startLatitude, startLongitude).then(services => {
+      const serviceNames = services.map(service => service.name)
+      const arrivalTimes = services.map(service => service.arrivalTime)
+      expect(serviceNames).to.eql(['uberPOOL', 'uberX', 'UberExec', 'ExecLarge', 'Taxi'])
+      expect(arrivalTimes).to.eql([360, 360, 300, 360, 300])
+      done()
+    })
+  })
+
+  it('should return with error message if uber is unreachable', (done) => {
+    nock('https://api.uber.com')
+      .get(`/v1.2/estimates/time?start_latitude=${startLatitude}&start_longitude=${startLongitude}`)
+      .replyWithError(500)
+
+    uberClient.getArrivalTime(startLatitude, startLongitude)
+      .catch(err => {
+        expect(err).to.eql('HTTP Call to Uber failed')
+        done()
+      })
+  })
+})
